358b7b3ff253818f8e7722b9570f229cf9fd6d36
AARONS-1 <751727@student.dusd.net>
2024-11-12 11:44:22 -0800

    did some code
    I made my7 character animate when moving and in process of fixing camera.

--- 2DBeginnerAdventureP4Serna/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP4Serna/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int maxHealth = 5;
    public int health { get { return currentHealth; } }
     int currentHealth;
@@ -17,11 +17,15 @@ public class PlayerController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
         
     }
 
@@ -30,6 +34,18 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
          vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
 
         if (isInvincible)
         {
@@ -52,6 +68,8 @@ public class PlayerController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return;
